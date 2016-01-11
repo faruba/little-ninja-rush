9d49f5f266fbef9fc205ac66ae7d4af70c579eb0
Ken <nukoug@gmail.com>
2016-01-11 10:39:30 +0800

    Reset Ref.cpp

--- cocos2d/cocos/base/CCRef.cpp
+++ cocos2d/cocos/base/CCRef.cpp
@@ -87,13 +87,6 @@ void Ref::retain()
 
 void Ref::release()
 {
-  //TODO why?
-  // i'm fucking tired
-  if( _referenceCount <= 0){
-    return;
-  }
-       //end
-  
     CCASSERT(_referenceCount > 0, "reference count should be greater than 0");
     --_referenceCount;
 
